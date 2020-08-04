c8d11f9d38b9a53cb94b10158476a50898579c31
iDiem <dylan.sr2002@gmail.com>
2020-08-04 16:13:57 -0500

    restserver-usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+let usuarioSchema = new Schema({
+    nombre:{
+        type:String,
+        required:[true,'El nombre es necesario']
+    },
+    email:{
+        type:String,
+        required:[true,'El Email es necesario'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'La contraseña es necesaria']
+    },
+    img:{
+        type:String,
+        required:false
+    },
+    rol:{
+        type:String,
+        default:'USER_ROLE',
+        enum:{
+            values:['ADMIN_ROLE','USER_ROLE'],
+            message:'{VALUE} no es un rol valido'
+        }
+    },
+    estado:{
+        type:Boolean,
+        default:true
+    },
+    google:{
+        type:Boolean,
+        default:false
+    }
+});
+
+usuarioSchema.methods.toJSON = function(){
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+};
+
+usuarioSchema.plugin(uniqueValidator,{
+    message : '{PATH} debe de ser unico'
+})
+module.exports =mongoose.model('usuario',usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,39 +1,24 @@
 require('./config/config')
 
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.URLDB,(err,db)=>{
+    if(err) throw err;
+    
+    console.log('BASE DE DATOS ONLINE');
+});
 
+const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
- 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario')
-})
-
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-
-    if(body.name===undefined){
-        res.status(400).json({
-            ok:false,
-            message:'El nombre es necesario'
-        })
-    }else{
-        res.json({body})
-    }
-})
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({id})
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-})
+app.use(require('./routes/usuario'));
  
 app.listen(process.env.PORT,()=>{
     console.log("Escuchando el puerto" + process.env.PORT);
